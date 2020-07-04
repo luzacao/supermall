13f7e153a4f9a5531ee59ff837e24f30e10cca31
luzacao <1565081157@qq.com>
2020-07-04 11:34:22 +0800

    初始化仓库

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -6,7 +6,8 @@
     <home-swiper :banners="banners" />
     <home-recommed-view :recommends="recommends"/>
     <feature-view/>  
-    <tab-control :titles="['流行','新款','经典']"/>
+    <tab-control :titles="['流行','新款','经典']" @tabClick="tabClick"/>
+    <goods-list :goods="showGoods"/>
 
   </div>
 </template>
@@ -17,6 +18,7 @@ import FeatureView from './childComps/FeatureView'
 
 import NavBar from "components/common/navbar/NavBar";
 import TabControl from "components/content/tabControl/TabControl"
+import GoodsList from 'components/content/goods/GoodsList'
 
 import {getHomeMultidata,getHomeGoods} from "network/home";
 export default {
@@ -26,7 +28,8 @@ export default {
     HomeRecommedView,
     FeatureView,
     NavBar,
-    TabControl
+    TabControl,
+    GoodsList
   },
   data() {
     return {
@@ -36,9 +39,15 @@ export default {
         'pop':{page:0,list:[]},
         'new':{page:0,list:[]},
         'sell':{page:0,list:[]},
-      }
+      },
+      currentType:'pop'
     };
   },
+ computed: {
+   showGoods(){
+     return this.goods[this.currentType].list
+   }
+ },
   created() {
     //1.请求多个数据
       this.getMultidata(),
@@ -47,7 +56,27 @@ export default {
       this.getGoods('new')
       this.getGoods('sell')
  },
+
  methods: {
+   /**
+    * 事件监听的方法
+    */
+    tabClick(index){
+      switch(index){
+        case 0:
+          this.currentType='pop'
+          break;
+        case 1:
+            this.currentType='new'
+          break;
+        case 2:
+          this.currentType='sell'
+          break;
+      }
+    },
+   /**
+    * 网络请求相关的方法
+    */
    getMultidata(){
       getHomeMultidata().then(res => {
       // console.log(res)
@@ -74,6 +103,12 @@ export default {
   left: 0;
   right: 0;
   top: 0;
+  z-index: 9;
+}
+.tab-control{
+  background-color: #fff;
+  position: sticky;
+  top:44px;
   z-index: 9;
 }
 #home{
